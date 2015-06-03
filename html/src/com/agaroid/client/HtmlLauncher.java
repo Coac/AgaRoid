ae190a52610fd80b5d8dc2e7569915175259e2db
SuperCoac <levictor3000@hotmail.fr>
2015-06-03 22:21:38 +0200

    Initial Commit - Libgdx Project

---
+++ html/src/com/agaroid/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.agaroid.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.agaroid.AgaRoid;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new AgaRoid();
+        }
+}
